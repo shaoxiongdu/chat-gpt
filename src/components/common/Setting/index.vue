000ed8534c1974b8b2d498e34656c1306df508aa
杜少雄 <dushaoxiong@lixiang.com>
2023-04-06 15:54:43 +0800

    feat: add typing effect (#1017) * feat: add typing effect
    * fix: ts2339 xxx not exist on type 'never'
    
    ---------
    
    Co-authored-by: ShaoxiongDu <email@shaoxiongdu.com>

--- src/components/common/Setting/index.vue
+++ src/components/common/Setting/index.vue
@@ -23,7 +23,7 @@ const authStore = useAuthStore()
 
 const isChatGPTAPI = computed<boolean>(() => !!authStore.isChatGPTAPI)
 
-const active = ref('General')
+const active = ref('Advanced')
 
 const show = computed({
   get() {
@@ -39,15 +39,15 @@ const show = computed({
   <NModal v-model:show="show" :auto-focus="false" preset="card" style="width: 95%; max-width: 640px">
     <div>
       <NTabs v-model:value="active" type="line" animated>
-        <NTabPane name="General" tab="General">
-          <template #tab>
-            <SvgIcon class="text-lg" icon="ri:file-user-line" />
-            <span class="ml-2">{{ $t('setting.general') }}</span>
-          </template>
-          <div class="min-h-[100px]">
-            <General />
-          </div>
-        </NTabPane>
+<!--        <NTabPane name="General" tab="General">-->
+<!--          <template #tab>-->
+<!--            <SvgIcon class="text-lg" icon="ri:file-user-line" />-->
+<!--            <span class="ml-2">{{ $t('setting.general') }}</span>-->
+<!--          </template>-->
+<!--          <div class="min-h-[100px]">-->
+<!--            <General />-->
+<!--          </div>-->
+<!--        </NTabPane>-->
         <NTabPane v-if="isChatGPTAPI" name="Advanced" tab="Advanced">
           <template #tab>
             <SvgIcon class="text-lg" icon="ri:equalizer-line" />
--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://avatars.githubusercontent.com/u/73691342?s=400&u=4f1667dba45b2f2f0a278981a8dbbcb60bf80225&v=4',
+      name: 'ShaoXiongDu',
+      description: ' <a href="https://shaoxiongdu.cn" class="text-blue-500" target="_blank" >个人博客</a>',
     },
   }
 }
